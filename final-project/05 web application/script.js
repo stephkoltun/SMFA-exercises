af25ade84facc1cd39148f693dc3bbd68cb4d235
stephkoltun <stephkoltun@icloud.com>
2015-04-11 11:53:50 -0400

    Get data via API and load graph after it's all returned
    Finally!!!

--- final-project/05 web application/script.js
+++ final-project/05 web application/script.js
@@ -1,49 +1,87 @@
-//Gets acquistion year and completetion year for specific object
 
 //Cooper Hewitt API token 
 var token = "2907bb23a319de02d7174829a85eef94";
 
-//Random Selection of Objects
+var allObjectsDataset = [];
 
+//Random Selection of Objects
 var objectsIDList = [
-			"18700327","18700329","18700427","18700463","18700465","18700487","18702279","18702283","18702573","18705893","18707303","35520811","35520817","51497205","51497673","52027699","68862903","18702819","18729905","18732835","18732295","68862901","35520793"
-			];
+			"18700327","18700329","18700427","18700463","18700465","18700487","18702279","18702283","18702573","18705893","18707303","35520811","35520817","51497205","51497673","52027699","68862903","18702819","18729905","18732835","18732295","68862901","35520793"];
+
+//run after function with callback function and length of object array
+var done = after(makeGraph, objectsIDList.length);
 
 for (i = 0; i < objectsIDList.length; i++) {
-
 	//get info about a specific object method - returns JSON
 	var url ='https://api.collection.cooperhewitt.org/rest/?method=cooperhewitt.objects.getInfo&access_token=' + token + '&object_id=' + objectsIDList[i];
+
+	// create an javascript obj for each object instance in collection
+	var objData = { 
+		objTitle: "tempTitle",
+		objYearStart:"tempDate", 
+		objYearEnd: "tempDate", 
+		//objDate: "tempDate",
+		objAcquired: "tempDate",
+	};
 
 	//AJAX request
 	var request = $.get(url, function( response ) {
 		var objResponse = response; // variable to hold the response
 		var obj = objResponse.object;
 
-		var objTitle = obj.title;		//variable for the title of the object
+		//console.log("Processing " + obj.title);
 
-		var objYearEnd = obj.year_end;	//variable for when object was finished
-		var objYearStart = obj.year_start;	//variable for when object was started
-		var objDate = obj.date.substring(0,4);
+		objData.objTitle = obj.title;	
+		objData.objYearEnd = obj.year_end;	//year object was finished
+		objData.objYearStart = obj.year_start;	//year object was started
+		//objData.objDate = obj.date.substring(0,4);
+		objData.objAcquired = obj.year_acquired;	//year object acquired
 
-		var objAcquired = obj.year_acquired;	//variable for acquisition date
+		allObjectsDataset.push(objData); // add object to full dataset
 
-		if (objYearEnd == "null" || objYearStart == "null") {
-			objYearEnd = objDate;
-			objYearStart = objDate;
-		}
+		// invoke done function to see if all objects have been processed
+		done();
 
-		var creationAge = objYearEnd - objYearStart;
-		var acquisitionAge = objAcquired - objYearStart;
-
-	
+		//var creationAge = objYearEnd - objYearStart;
+		//var acquisitionAge = objAcquired - objYearStart;
 		
-		//var $msg = "<p>" + objTitle + ", created in " + objDate + ", was aquired by the Cooper Hewitt in " + objAcquired + ".</p>";
+		/*//var $msg = "<p>" + objTitle + ", created in " + objDate + ", was aquired by the Cooper Hewitt in " + objAcquired + ".</p>";
 		var $msg = "<h3>" + objTitle + "</h3><br><p>Created in " + objYearStart + " - " + objYearEnd + "<br>Acquired in " + objAcquired + "<br>Creation Lifespan: " + creationAge + "<br>Acquision Lifespan: " + acquisitionAge;
-		$('#test').after($msg);
-
-	});
-
+		$('#test').after($msg);*/
+	}); //End AJAX request
 }
+
+
+function makeGraph() {
+	var width = 800;
+	var height = 600;
+	var padding = 55;
+
+	// create SVG
+	var svg = d3.select("#graph")
+				.append("svg")
+				.attr("width", width)
+				.attr("height", height);
+
+	var data = allObjectsDataset; //global variable
+	console.log(data);
+}
+
+// check if all data has been looped through and returned from API
+// checks if counter is equal to the length of objects being looped through
+function after(callback, count){
+	var counter = 0;
+	return function(){
+		if(++counter === count) {
+			counter = 0;
+			callback();
+		}
+	};
+}
+
+
+
+
 
 
 
